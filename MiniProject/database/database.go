a20993668a08a51e1570ee76350967ab549f4a1a
Muhammad177 <wahyuyudiansyah20@gmail.com>
2023-05-10 07:13:30 +0700

    Deploy

--- MiniProject/database/database.go
+++ MiniProject/database/database.go
@@ -40,7 +40,7 @@ func InitDB() {
 		DB_Username: "wahyu",
 		DB_Password: "",
 		DB_Port:     "3306",
-		DB_Host:     "192.168.100.162",
+		DB_Host:     "127.0.0.1",
 		DB_Name:     "miniproject",
 	}
 
